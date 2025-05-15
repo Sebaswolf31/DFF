ac9d669210e1725ba844116eaada978fa4b71b8c
Sebaswolf31 <sebasunivercal@hotmail.com>
2025-05-15 12:16:32 -0500

    devolviendo cambios

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    screens: {
-      xs: '375px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-    },
     extend: {
       fontFamily: {
         sans: ["'Nunito'", 'sans-serif'],
@@ -21,22 +15,8 @@ export default {
         blueP: '#175699',
         blueLight: '#188FA3',
         greenP: '#41B495',
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
-      spacing: {
-        'mobile-pad': '4vw',
-        'desktop-pad': '8vw',
       },
     },
   },
-  plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
-        '.text-balance': {
-          'text-wrap': 'balance',
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
